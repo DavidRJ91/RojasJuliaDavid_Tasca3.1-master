6927c6ee38fbd6ba2d7355761311a14b1334d7d1
David Rojas Julia <drj00@iesemilidarder.com>
2017-10-11 19:53:09 +0200

    terminado

--- src/Nprimos.java
+++ src/Nprimos.java
@@ -1,21 +1,18 @@
 public class Nprimos {
     public static void main(String[] args) {
-        int x = 2;
-        int z = 10000;
         boolean Nprimo;
-
-        for (int i = x; i <= z; i++) {
+        for (int i = 1; i <= 10000; i++) {
             Nprimo = true;
 
-            for ( ; x<= i; x++)
-                {if (i % x == 0) {
+            for (int divisor = i-1; divisor>=2; divisor--) {
+                if (i % divisor == 0) {
                     Nprimo = false;
                 }
             }
             if (Nprimo) {
                 System.out.println(i);
             }
-            if ( i % 1000 == 0) {
+            if (i % 1000 == 0) {
                 System.out.println("<<<<--------------->>>>");
             }
         }
